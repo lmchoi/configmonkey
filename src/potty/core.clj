e33833a44755e1dadce7ffbfe2c83fe1631260a5
Mandy Choi <hello@mandychoi.com.au>
2017-03-17 18:55:10 +0000

    munch first

--- src/potty/core.clj
+++ src/potty/core.clj
@@ -1,10 +1,9 @@
 (ns potty.core
   (:gen-class)
-  (:require [potty.required-attributes-finder :as finder]
-            [potty.stripper :as extractor]))
+  (:require [potty.config-muncher :as muncher]))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [filename & args]
-  (prn  (-> (finder/find-attributes-deprecated (str "resources/" filename ".erb"))
-            (extractor/extract-values-deprecated (str "resources/" filename)))))
+  [& args]
+  (prn (muncher/munch {:input "resources/"
+                       :output "output/"})))
