31af159120adb3bb2ae2f4bcd98842993fe1bf6b
Mandy Choi <hello@mandychoi.com.au>
2017-03-22 00:25:40 +0000

    implemented placeholder parser

--- src/com/lmc/monkey/config/chef_recipe_generator.clj
+++ src/com/lmc/monkey/config/chef_recipe_generator.clj
@@ -1,8 +1,17 @@
-(ns com.lmc.monkey.config.chef-recipe-generator)
+(ns com.lmc.monkey.config.chef-recipe-generator
+  (:require [clojure.string :as str]))
 
 (defn- parse-placeholders
   [placeholders]
-  (str "override" (second (re-matches #"<%=\s*node(\[.+\])\s*%>" placeholders))))
+  (let [placeholder-matches (re-seq #"(.*?)(<%=\s*node\[.+?\]\s*%>)|(.+)?" placeholders)]
+    (mapcat
+     #(filter not-empty (rest %)) placeholder-matches)
+    #_(when ph
+      (clojure.string/replace ph #"node" "override"))))
+
+(defn- is-placeholder?
+  [str]
+  (re-find #"<%=\s*node\[.+?\]\s*%>" str))
 
 (defn- per-attribute
   [values [fieldname placeholders]]
@@ -14,3 +23,26 @@
 
 (defn generate-attributes [{:keys [template values]}]
   (into {} (map #(per-env % (:erb template)) values)))
+
+(defn find-values
+  [templated-value placeholders]
+  ; TODO flag a warning if there is more than one placeholders in a str
+  (loop [ret []
+         str-values templated-value
+         p-holders placeholders]
+    (if p-holders
+      (let [placeholder (first p-holders)]
+        (cond
+          (is-placeholder? placeholder)
+          (recur (conj ret [placeholder str-values])
+                 (str/replace-first str-values placeholder "")
+                 (next p-holders))
+          (str/starts-with? str-values placeholder)
+          (recur ret
+                 (str/replace-first str-values placeholder "")
+                 (next p-holders))
+          :default
+          (recur ret
+                 str-values
+                 (next p-holders))))
+      ret)))
--- test/com/lmc/monkey/config/chef_recipe_generator_test.clj
+++ test/com/lmc/monkey/config/chef_recipe_generator_test.clj
@@ -2,8 +2,62 @@
   (:use midje.sweet)
   (:require [com.lmc.monkey.config.chef-recipe-generator :as chef]))
 
-(facts "about generating attributes"
+(use '[midje.util :only [testable-privates]])
+
+(facts "about parsing placeholders"
+       (testable-privates com.lmc.monkey.config.chef-recipe-generator parse-placeholders)
+       (fact "empty string"
+             (parse-placeholders "")
+             => [])
+       (fact "incomplete placeholders are treated as a string"
+             (parse-placeholders "<%= blah")
+             => ["<%= blah"])
+       (fact "string containing no placeholders are treated as a single string"
+             (parse-placeholders "contains-no-placeholders")
+             => ["contains-no-placeholders"])
+       (fact "contains one singler-level placeholder by itself"
+             (parse-placeholders "<%= node['placeholder'] %>")
+             => ["<%= node['placeholder'] %>"])
+       (fact "contains one multi-level placeholder by itself"
+             (parse-placeholders "<%= node['placeholder']['sub1']['sub2'] %>")
+             => ["<%= node['placeholder']['sub1']['sub2'] %>"])
+       (fact "placeholder with a string before it"
+             (parse-placeholders "some-string <%= node['placeholder'] %>")
+             => ["some-string " "<%= node['placeholder'] %>"])
+       (fact "placeholder with a string after it"
+             (parse-placeholders "<%= node['placeholder'] %> some-string")
+             => ["<%= node['placeholder'] %>" " some-string"])
+       (fact "placeholder with a string after it"
+             (parse-placeholders "http://<%= node['host'] %>:<%= node['port'] %>/some-path")
+             => ["http://" "<%= node['host'] %>" ":" "<%= node['port'] %>" "/some-path"]))
+
+#_(facts "about finding the value of placeholders"
+       (fact "single value"
+             (chef/find-values 1337 ["<%= node['port'] %>"])
+             => [["<%= node['port'] %>" 1337]])
+
+       ; log error?
+       (fact "ignore if it is not a placeholder"
+             (chef/find-values 1337 ["not-a-placeholder"])
+             => [])
+
        (fact ""
+             (chef/find-values
+              "http://localhost:8080/some-path"
+              ["http://" "<%= node['host'] %>" ":" "<%= node['port'] %>" "/some-path"])
+             => [["<%= node['host'] %>" "localhost"]
+                 ["<%= node['port'] %>" 8080]]))
+#_(facts "about generating attributes"
+       (fact "no matching fields"
+             (chef/generate-attributes
+              {:template {:erb {:host "<%= node['local']['host'] %>",
+                                :port "<%= node['local']['port'] %>"}}
+               :values   {:dev {:host "munchy.dev"
+                                :port 1337}
+                          :qa  {:host "munchy.qa"
+                                :port 9001}}}
+              ))
+       (fact "single attribute fields"
              (chef/generate-attributes
               {:template {:erb {:host "<%= node['local']['host'] %>",
                                 :port "<%= node['local']['port'] %>"}}
@@ -11,6 +65,16 @@
                                 :port 1337}
                           :qa  {:host "munchy.qa"
                                 :port 9001}}})
+             => {:dev {"override['local']['host']" "munchy.dev"
+                       "override['local']['port']" 1337}
+                 :qa  {"override['local']['host']" "munchy.qa"
+                       "override['local']['port']" 9001}})
+
+       (fact "field composed of multiple attributes"
+             (chef/generate-attributes
+              {:template {:erb {:url "http://<%= node['local']['host'] %>:<%= node['local']['port'] %>"}}
+               :values   {:dev {:url "http://munchy.dev:1337"}
+                          :qa  {:url "http://munchy.qa:9001"}}})
              => {:dev {"override['local']['host']" "munchy.dev"
                        "override['local']['port']" 1337}
                  :qa  {"override['local']['host']" "munchy.qa"
